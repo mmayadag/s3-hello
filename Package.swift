6fc97f316bd58deac3741587d9491218a8204213
Murat Mayadağ <murat-2@hotmail.com>
2019-12-02 11:28:19 +0300

    :tada: initial

--- Package.swift
+++ Package.swift
@@ -1,12 +1,19 @@
-// swift-tools-version:3.1
+// swift-tools-version:5.1
 
 import PackageDescription
+
 
 let package = Package(
     name: "hello",
     dependencies: [
-        .Package(url: "https://github.com/IBM-Swift/Kitura.git", majorVersion: 1, minor: 7),
-        .Package(url: "https://github.com/IBM-Swift/HeliumLogger.git", majorVersion: 1, minor: 7),     
-        .Package(url: "https://github.com/PerfectlySoft/Perfect-Crypto.git", majorVersion: 1),
+        .package(url: "https://github.com/IBM-Swift/Kitura.git",from:"2.9.1"),
+        .package(url: "https://github.com/IBM-Swift/HeliumLogger.git", from:"1.9.0"),
+        .package(url: "https://github.com/PerfectlySoft/Perfect-Crypto.git",from:"3.2.0")
+    ],
+    targets:[
+        .target(
+            name:"hello",
+            dependencies:["Kitura","HeliumLogger","PerfectCrypto"]
+        )
     ]
 )
